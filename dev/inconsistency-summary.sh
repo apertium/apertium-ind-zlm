eb1c6b5d088457b3ff97cdbf4d868d2ad4ecf9ac
Raymond HS <raymhs91@gmail.com>
2012-06-25 10:52:23 +0000

    coverage, testvoc

--- dev/inconsistency-summary.sh
+++ dev/inconsistency-summary.sh
@@ -1,6 +1,6 @@
 INC=$1
 OUT=testvoc-summary.$2.txt
-POS="adj adv cnjcoo det ij n np num pr prn vblex vaux cnjsub"
+POS="adj adv cnjadv cnjcoo cnjsub det ij n np num pr prn vblex"
 
 ECHOE="echo -e"
 SED=sed
@@ -17,42 +17,30 @@ date >> $OUT
 $ECHOE  "===============================================" >> $OUT
 $ECHOE  "POS	Total	Clean	With @	With #	Clean %" >> $OUT
 for i in $POS; do
-	if [ "$i" = "det" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<n>' -e '<np>' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<n>' -e '<np>'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v -e '<n>' -e '<np>' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "preadv" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<adj>' -e '<adv>' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<adj>' -e '<adv>'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v -e '<adj>' -e '<adv>' | grep -v REGEX |  wc -l`;
+	if [ "$i" = "n" ]; then
+		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<n><vblex>|<n><adj>/' | wc -l`; 
+		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<n><vblex>|<n><adj>/' | wc -l`;
+		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<n><vblex>|<n><adj>/' |  wc -l`;
+	elif [ "$i" = "vblex" ]; then
+		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<vblex><n>/' | wc -l`; 
+		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<vblex><n>/' | wc -l`;
+		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<vblex><n>/' | wc -l`;
 	elif [ "$i" = "adj" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<part>' -e '<qst>' -e '<cnj' -e '<v'  | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<part>' -e '<qst>' -e '<cnj' -e '<v'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v -e '<part>' -e '<qst>' -e '<cnj' -e '<v' | grep -v REGEX |  wc -l`;
+		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`; 
+		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`;
+		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`;
 	elif [ "$i" = "adv" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<part>' -e '<qst>' -e '<cnj' -e '<v' -e '<adj>' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<part>' -e '<qst>' -e '<cnj' -e '<v' -e '<adj>'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v -e '<part>' -e '<qst>' -e '<cnj' -e '<v' -e '<adj>' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "cnjcoo" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<v>' -e '<n>' -e '<adj>' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<v>' -e '<n>' -e '<adj>' | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v -e '<v>' -e '<n>' -e '<adj>' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "np" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<part>' -e '<qst>' -e '<cnj' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<part>' -e '<qst>' -e '<cnj'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v -e '<part>' -e '<qst>' -e '<cnj' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "n" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<part>' -e '<qst>' -e '<cnj' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<part>' -e '<qst>' -e '<cnj'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v -e '<part>' -e '<qst>' -e '<cnj' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "v" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<part>' -e '<qst>' -e '<cnj' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<part>' -e '<qst>' -e '<cnj'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v -e '<part>' -e '<qst>' -e '<cnj' | grep -v REGEX |  wc -l`;
+		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`; 
+		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`;
+		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`;
+	elif [ "$i" = "det" ]; then
+		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<det><n>|<det><vblex>/' | wc -l`; 
+		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<det><n>|<det><vblex>/' | wc -l`;
+		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<det><n>|<det><vblex>/' | wc -l`;
 	else
-		TOTAL=`cat $INC | grep "<$i>" | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" |  sed 's/~#/#/g' |grep '>  *#' | grep -v REGEX |  wc -l`;
+		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | wc -l`; 
+		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | wc -l`;
+		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | wc -l`;
 	fi
 	UNCLEAN=`calc $AT+$HASH`;
 	CLEAN=`calc $TOTAL-$UNCLEAN`;
