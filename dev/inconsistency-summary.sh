4f6c1b4047760e0ff64de4cb5de35584a7de4bd7
Raymond HS <raymhs91@gmail.com>
2012-06-27 18:29:50 +0000

    Determiners, testvoc

--- dev/inconsistency-summary.sh
+++ dev/inconsistency-summary.sh
@@ -18,29 +18,29 @@ $ECHOE  "===============================================" >> $OUT
 $ECHOE  "POS	Total	Clean	With @	With #	Clean %" >> $OUT
 for i in $POS; do
 	if [ "$i" = "n" ]; then
-		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<n><vblex>|<n><adj>/' | wc -l`; 
-		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<n><vblex>|<n><adj>/' | wc -l`;
-		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<n><vblex>|<n><adj>/' |  wc -l`;
+		TOTAL=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | awk -F'\t' '$1 !~ /<n><vblex>|<n><adj>/' | wc -l`; 
+		AT=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | grep '@' | awk -F'\t' '$1 !~ /<n><vblex>|<n><adj>/' | wc -l`;
+		HASH=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk -F'\t' '$1 !~ /<n><vblex>|<n><adj>/' |  wc -l`;
 	elif [ "$i" = "vblex" ]; then
-		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<vblex><n>/' | wc -l`; 
-		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<vblex><n>/' | wc -l`;
-		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<vblex><n>/' | wc -l`;
+		TOTAL=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | awk -F'\t' '$1 !~ /<vblex><n>/' | wc -l`; 
+		AT=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | grep '@' | awk -F'\t' '$1 !~ /<vblex><n>/' | wc -l`;
+		HASH=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk -F'\t' '$1 !~ /<vblex><n>/' | wc -l`;
 	elif [ "$i" = "adj" ]; then
-		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`; 
-		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`;
-		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`;
+		TOTAL=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | awk -F'\t' '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`; 
+		AT=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | grep '@' | awk -F'\t' '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`;
+		HASH=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk -F'\t' '$1 !~ /<adj><n>|<adj><vblex>/' | wc -l`;
 	elif [ "$i" = "adv" ]; then
-		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`; 
-		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`;
-		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`;
+		TOTAL=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | awk -F'\t' '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`; 
+		AT=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | grep '@' | awk -F'\t' '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`;
+		HASH=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk -F'\t' '$1 !~ /<adv><n>|<adv><vblex>/' | wc -l`;
 	elif [ "$i" = "det" ]; then
-		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | awk '$1 !~ /<det><n>|<det><vblex>/' | wc -l`; 
-		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | awk '$1 !~ /<det><n>|<det><vblex>/' | wc -l`;
-		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk '$1 !~ /<det><n>|<det><vblex>/' | wc -l`;
+		TOTAL=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | awk -F'\t' '$1 !~ /<det><n>|<det><vblex>/' | wc -l`; 
+		AT=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | grep '@' | awk -F'\t' '$1 !~ /<det><n>|<det><vblex>/' | wc -l`;
+		HASH=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' |  grep '#' | awk -F'\t' '$1 !~ /<det><n>|<det><vblex>/' | wc -l`;
 	else
-		TOTAL=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | wc -l`; 
-		AT=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' | grep '@' | wc -l`;
-		HASH=`cat $INC | awk -v tag="<$i>" '$1 ~ tag' |  grep '#' | wc -l`;
+		TOTAL=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | wc -l`; 
+		AT=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' | grep '@' | wc -l`;
+		HASH=`cat $INC | awk -F'\t' -v tag="<$i>" '$1 ~ tag' |  grep '#' | wc -l`;
 	fi
 	UNCLEAN=`calc $AT+$HASH`;
 	CLEAN=`calc $TOTAL-$UNCLEAN`;
@@ -53,7 +53,7 @@ for i in $POS; do
 	fi
 
 	$ECHOE $TOTAL";"$i";"$CLEAN";"$AT";"$HASH";"$TOTPERCLEAN;
-done | sort -gr | awk -F';' '{print $2"\t"$1"\t"$3"\t"$4"\t"$5"\t"$6}' >> $OUT
+done | sort -gr | awk -F'\t' -F';' '{print $2"\t"$1"\t"$3"\t"$4"\t"$5"\t"$6}' >> $OUT
 
 $ECHOE "===============================================" >> $OUT
 cat $OUT;
