70fd98ded16d84699772c16c81ada4b99cfbdda1
Marc Riera Irigoyen <marc.riera.irigoyen@gmail.com>
2018-06-25 08:12:34 +0200

    Upgrade to work with monolingual packages

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -1,20 +1,20 @@
 TMPDIR=/tmp
 
-if [[ $1 = "id-ms" ]]; then
+if [[ $1 = "ind-zlm" ]]; then
 
-lt-expand ../apertium-id-ms.id.dix | grep -v "REGEXP" | sort -u |  sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../../apertium-ind/apertium-ind.ind.dix | grep -v "REGEXP" | sort -u |  sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
 	apertium-pretransfer |
-	apertium-transfer ../apertium-id-ms.id-ms.t1x  ../id-ms.t1x.bin  ../id-ms.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt | 
-	lt-proc -d ../id-ms.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+	apertium-transfer ../apertium-ind-zlm.ind-zlm.t1x  ../ind-zlm.t1x.bin  ../ind-zlm.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt | 
+	lt-proc -d ../ind-zlm.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 
 paste $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g'
 
-elif [[ $1 = "ms-id" ]]; then
+elif [[ $1 = "zlm-ind" ]]; then
 
-lt-expand ../apertium-id-ms.ms.dix | grep -v "REGEXP" | sort -u | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../../apertium-zlm/apertium-zlm.zlm.dix | grep -v "REGEXP" | sort -u | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
 	apertium-pretransfer |
-	apertium-transfer ../apertium-id-ms.ms-id.t1x  ../ms-id.t1x.bin  ../ms-id.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
-	lt-proc -d ../ms-id.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+	apertium-transfer ../apertium-ind-zlm.zlm-ind.t1x  ../zlm-ind.t1x.bin  ../zlm-ind.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+	lt-proc -d ../zlm-ind.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 
 paste $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g'
 
